ffbd2c25c5787be2b9e65ec3f1d140e0462c18ae
smenon <smenon@b7fb9f19-7815-0410-a869-ec1e8dcad60f>
2009-08-24 20:44:31 +0000

    Adding viscoInterTrackFoam for the first time.
    git-svn-id: svn+ssh://cod/usr/local/svnrepo/apps/Sandeep/trunk/surfaceTracking@1162 b7fb9f19-7815-0410-a869-ec1e8dcad60f

---
+++ interfaceTrackFoam/viscoInterTrackFoam.C
@@ -0,0 +1,218 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+Application
+    viscoInterTrackFoam
+
+Description
+    Incompressible laminar CFD code for interface between fluid phases using
+    a dynamic mesh and visco-elastic effects.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "viscoelasticModel.H"
+#include "viscosityModel.H"
+#include "dynamicTopoFvMesh.H"
+#include "fluidInterface.H"
+
+// Mesh motion solvers
+#include "motionSolver.H"
+#include "tetDecompositionMotionSolver.H"
+#include "faceTetPolyPatch.H"
+#include "tetPolyPatchInterpolation.H"
+#include "setMotionBC.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createDynamicMesh.H"
+#   include "initContinuityErrs.H"
+#   include "initTotalVolume.H"
+#   include "createFields.H"
+
+    viscoelasticModel visco(U, phi); 
+
+    fluidInterface interface(mesh, U, p, phi);
+
+    Info<< "Reading field rUA if present\n" << endl;
+    volScalarField rUA
+    (
+        IOobject
+        (
+            "rUA",
+            runTime.timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        runTime.deltaT()/interface.rho()
+    );
+
+    Info << "\nStarting time loop\n" << endl;
+
+    // Initialize the motion solver
+    autoPtr<motionSolver> mPtr = motionSolver::New(mesh);
+
+    while (runTime.run())
+    {
+#       include "readPISOControls.H"
+#       include "readTimeControls.H"
+#       include "checkTotalVolume.H"
+#       include "CourantNo.H"
+#       include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        // Update the interface
+        interface.updateInterface();
+
+        setMotionBC(mesh, interface.aPatchID(), interface.displacement());
+
+        if (interface.twoFluids())
+        {
+            // Interpolate displacement to the shadow patch
+            pointField dispB = interface.interpolatorAB().pointInterpolate
+                               (
+                                   interface.displacement()
+                               );
+
+            setMotionBC(mesh, interface.bPatchID(), dispB);
+        }
+
+        // Solve for motion
+        mesh.movePoints(mPtr->newPoints());
+
+#       include "volContinuity.H"
+
+        for (int corr=0; corr<nOuterCorr; corr++)
+        {
+            // Update boundary conditions on velocity and pressure
+            interface.updateBoundaryConditions();
+
+            // Make the fluxes relative to the mesh motion
+            fvc::makeRelative(phi, interface.rho(), U);
+
+            // Momentum predictor
+            fvVectorMatrix UEqn
+            (
+                fvm::ddt(interface.rho(), U)
+              + fvm::div(fvc::interpolate(interface.rho())*phi, U)
+              - interface.rho()*visco.divTau(U)
+            );
+
+            solve(UEqn == -fvc::grad(p));
+
+            rUA = 1.0/UEqn.A();
+
+            // --- PISO loop
+
+            for (int corr=0; corr<nCorr; corr++)
+            {
+                U = rUA*UEqn.H();
+
+                phi = (fvc::interpolate(U) & mesh.Sf());
+
+                adjustPhi(phi, U, p);
+
+                for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
+                {
+                    fvScalarMatrix pEqn
+                    (
+                        fvm::laplacian(rUA, p) == fvc::div(phi)
+                    );
+
+                    pEqn.setReference(pRefCell, pRefValue);
+
+                    if (corr == nCorr - 1 && nonOrth == nNonOrthCorr)
+                    {
+                        pEqn.solve(mesh.solver(p.name() + "Final"));
+                    }
+                    else
+                    {
+                        pEqn.solve(mesh.solver(p.name()));
+                    }
+
+                    if (nonOrth == nNonOrthCorr)
+                    {
+                        phi -= pEqn.flux();
+                    }
+                }
+
+#               include "continuityErrs.H"
+
+                // Make the fluxes relative
+                fvc::makeRelative(phi, interface.rho(), U);
+
+                U -= rUA*fvc::grad(p);
+                U.correctBoundaryConditions();
+
+                visco.correct();
+            }
+
+#           include "freeSurfaceContinuityErrs.H"
+
+            Info << endl;
+        }
+
+        // Make the fluxes absolute
+        fvc::makeAbsolute(phi, interface.rho(), U);
+
+        bool meshChanged = mesh.updateTopology();
+
+        if (meshChanged)
+        {
+#           include "checkTotalVolume.H"
+            phi = (fvc::interpolate(U) & mesh.Sf());
+#           include "correctPhi.H"
+#           include "CourantNo.H"
+
+            // Update the interface
+            interface.updateMesh(mesh.meshMap());
+
+            // Update the motion solver
+            mPtr->updateMesh(mesh.meshMap());
+        }
+
+        Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+             << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+             << nl << endl;
+
+        runTime.write();
+
+#       include "meshInfo.H"
+    }
+
+    Info<< "End\n" << endl;
+
+    return(0);
+}
+
+// ************************************************************************* //
