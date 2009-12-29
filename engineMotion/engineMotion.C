4ef462fa8e47d52c81467c7e084329ef1d687e81
smenon <smenon@b7fb9f19-7815-0410-a869-ec1e8dcad60f>
2009-12-29 17:28:36 +0000

    Formatting changes.
    git-svn-id: svn+ssh://cod/usr/local/svnrepo/apps/Sandeep/trunk/surfaceTracking@1234 b7fb9f19-7815-0410-a869-ec1e8dcad60f

--- engineMotion/engineMotion.C
+++ engineMotion/engineMotion.C
@@ -62,30 +62,30 @@ int main(int argc, char *argv[])
             IOobject::NO_WRITE
         )
     );
-    
+
     // Obtain the piston patch name
     dictionary piston(engineDict.subDict("piston"));
     wordList pistonName = piston.toc();
-    
+
     // Read in necessary information from the dictionary
     dictionary pistonDict = engineDict.subDict(pistonName[0]);
-    
+
     // Read the profile
     interpolationTable<scalar> pistonTable
     (
         fileName(pistonDict.lookup("profileFile"))
     );
-    
+
     // Read in the piston axis
     vector pistonAxis = pistonDict.lookup("axis");
-    
+
     pistonAxis /= mag(pistonAxis) + VSMALL;
-    
+
     scalar oldStroke = pistonTable(runTime.value()), currentStroke = 0.0;
-    
+
     // Obtain the number of valves in the system
     label numValves = readLabel(engineDict.lookup("numValves"));
-    
+
     PtrList<interpolationTable<scalar> > valveLiftTables(numValves);
     List<scalar> oldLift(numValves, 0.0), currentLift(numValves, 0.0);
     List<vector> valveAxes(numValves, vector::zero);
@@ -97,10 +97,10 @@ int main(int argc, char *argv[])
     // Read in all necessary information from the dictionary
     forAll(valveList, valveI)
     {
-        dictionary valveDict = engineDict.subDict(valveList[valveI]);        
-      
+        dictionary valveDict = engineDict.subDict(valveList[valveI]);
+
         // Read in the lift profile table
-        valveLiftTables.set 
+        valveLiftTables.set
         (
             valveI,
             new interpolationTable<scalar>
@@ -108,76 +108,79 @@ int main(int argc, char *argv[])
                 fileName(valveDict.lookup("profileFile"))
             )
         );
-        
+
         // Read in the valve axis
         valveAxes[valveI] = valveDict.lookup("axis");
-        
+
         // Normalize the axis
         valveAxes[valveI] /= mag(valveAxes[valveI]) + VSMALL;
-        
+
         // Interpolate and obtain the lift value
         // for the current time-step
         oldLift[valveI] = valveLiftTables[valveI](runTime.value());
     }
-    
+
     const polyBoundaryMesh& boundary = mesh.boundaryMesh();
 
     for (runTime++; !runTime.end(); runTime++)
     {
         Info << "Time = " << runTime.value() << endl << endl;
-        
+
         // Copy existing point locations
         pointField meshPoints(mesh.points());
-        
+
         // Update the piston
         currentStroke = pistonTable(runTime.value());
-        
-        const labelList& pistonPoints = 
+
+        const labelList& pistonPoints =
         (
             boundary[boundary.findPatchID(pistonName[0])].meshPoints()
         );
-        
+
         vector p = (currentStroke - oldStroke)*pistonAxis;
-        
-        Info << "Piston " << pistonName[0] << ": Current Stroke value: " 
-             << currentStroke << endl;
-        Info << "Piston " << pistonName[0] << ": Old Stroke value: " 
-             << oldStroke << endl;
-        
+
+        Info << "Piston " << pistonName[0]
+             << ": Current Stroke value: " << currentStroke << endl;
+        Info << "Piston " << pistonName[0]
+             << ": Old Stroke value: " << oldStroke << endl;
+
         forAll(pistonPoints, index)
         {
             meshPoints[pistonPoints[index]] += p;
         }
-        
+
         oldStroke = currentStroke;
+
+        vector v = vector::zero;
 
         // Update valves
         forAll(valveList, valveI)
         {
             // Obtain the current lift value
             currentLift[valveI] = valveLiftTables[valveI](runTime.value());
-          
+
             // Move the valve patch by the difference in lift
-            const labelList& valvePoints = 
+            const labelList& valvePoints =
             (
                 boundary[boundary.findPatchID(valveList[valveI])].meshPoints()
             );
-            
-            vector v = (currentLift[valveI] - oldLift[valveI])*valveAxes[valveI];
-            
-            Info << "Valve " << valveList[valveI] << ": Current Lift value: " 
-                 << currentLift[valveI] << endl;
-            Info << "Valve " << valveList[valveI] << ": Old Lift value: " 
-                 << oldLift[valveI] << endl;
-            
+
+            // Define displacement
+            v = (currentLift[valveI] - oldLift[valveI])*valveAxes[valveI];
+
+            Info << "Valve " << valveList[valveI]
+                 << ": Current Lift value: " << currentLift[valveI] << endl;
+            Info << "Valve " << valveList[valveI]
+                 << ": Old Lift value: " << oldLift[valveI] << endl;
+
             forAll(valvePoints, index)
             {
                 meshPoints[valvePoints[index]] += v;
             }
-            
+
             oldLift[valveI] = currentLift[valveI];
         }
-        
+
         // Obtain the field from the registry
         pointField& refPoints = const_cast<pointField&>
         (
