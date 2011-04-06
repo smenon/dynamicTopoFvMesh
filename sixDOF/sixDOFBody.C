3837b4d3ce98d74f44aa99595f056e6f456e3b6a
Sandeep Menon <smenon@ecs.umass.edu>
2011-04-06 13:44:13 -0400

    Initial commit:
     - Checking in an initial commit for sixDOFFoam, based on shipFoam

---
+++ sixDOF/sixDOFBody.C
@@ -0,0 +1,453 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Class
+    sixDOFBody
+
+Description
+    Six DOF Body
+
+Author
+    Sandeep Menon
+    University of Massachusetts Amherst
+    All rights reserved
+
+\*---------------------------------------------------------------------------*/
+
+#include "sixDOFBody.H"
+#include "ODESolver.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "mathematicalConstants.H"
+#include "uniformDimensionedFields.H"
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+//- Calculate transformation tensor
+//   - Assumes rot is in radians
+tensor sixDOFBody::calcTensor(const vector& rot) const
+{
+    scalar crX = Foam::cos(rot.x()), srX = Foam::sin(rot.x());
+    scalar crY = Foam::cos(rot.y()), srY = Foam::sin(rot.y());
+    scalar crZ = Foam::cos(rot.x()), srZ = Foam::sin(rot.x());
+
+    tensor Rx
+    (
+        1.0, 0.0, 0.0,
+        0.0, crX,-srX,
+        0.0, srX, crX
+    );
+
+    tensor Ry
+    (
+        crY, 0.0, srY,
+        0.0, 1.0, 0.0,
+       -srY, 0.0, crY
+    );
+
+    tensor Rz
+    (
+        crZ,-srZ, 0.0,
+        srZ, crZ, 0.0,
+        0.0, 0.0, 1.0
+    );
+
+    // Return rotation matrix
+    return (Rz & Ry & Rx);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+// Construct from components
+sixDOFBody::sixDOFBody
+(
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const dictionary& bodyDict,
+    const label index
+)
+:
+    mesh_(mesh),
+    dict_(dict),
+    bodyDict_(bodyDict),
+    index_(index),
+    patchIndex_
+    (
+        mesh.boundaryMesh().findPatchID
+        (
+            bodyDict.lookup("patchName")
+        )
+    ),
+    weightFactor_(bodyDict.lookup("weightFactor")),
+    localVelocity_(vector::zero),
+    localOldVelocity_
+    (
+        bodyDict.lookupOrDefault
+        (
+            "initialVelocity",
+            vector::zero
+        )
+    ),
+    globalVelocity_(vector::zero),
+    localOmega_(vector::zero),
+    localOldOmega_
+    (
+        bodyDict.lookupOrDefault
+        (
+            "initialAngularVelocity",
+            vector::zero
+        )
+    ),
+    globalOmega_(vector::zero),
+    displacement_(vector::zero),
+    totalDisplacement_(vector::zero),
+    rotation_(vector::zero),
+    totalRotation_(vector::zero),
+    fAvg_(vector::zero),
+    mAvg_(vector::zero),
+    fTotal_(vector::zero),
+    Fs_(vector::zero),
+    Ms_(vector::zero),
+    aMin_(bodyDict.lookupOrDefault("minAcceleration", -GREAT)),
+    aMax_(bodyDict.lookupOrDefault("maxAcceleration", GREAT)),
+    Cg_(bodyDict.lookup("CentreOfGravity")),
+    Rot_(bodyDict.lookup("Rotation")),
+    calcTransDOF_(bodyDict.lookup("calcTranslationDOF")),
+    calcRotDOF_(bodyDict.lookup("calcRotationDOF")),
+    fConst_
+    (
+        bodyDict.lookupOrDefault
+        (
+            "constantForce",
+            vector::zero
+        )
+    ),
+    mConst_
+    (
+        bodyDict.lookupOrDefault
+        (
+            "constantMoment",
+            vector::zero
+        )
+    ),
+    mass_(readScalar(bodyDict.lookup("mass"))),
+    J_(bodyDict.lookup("MomentOfInertia")),
+    kTrans_(bodyDict.lookup("translationSpringConstant")),
+    dTrans_(bodyDict.lookup("translationDampingConstant")),
+    kRot_(bodyDict.lookup("rotationSpringConstant")),
+    dRot_(bodyDict.lookup("rotationDampingConstant")),
+    pName_(bodyDict.lookupOrDefault("pName", word("p"))),
+    gName_(bodyDict.lookupOrDefault("gName", word("g")))
+{
+    // Load the ODE solver
+    odeSolver_ =
+    (
+        ODESolver::New
+        (
+            dict_.subDict("ODECoeffs").lookup("ODESolver"),
+            ode_
+        )
+    );
+
+    // Read ODE options
+    eps_ = readScalar(dict_.subDict("ODECoeffs").lookup("eps"));
+    hEst_ = readScalar(dict_.subDict("ODECoeffs").lookup("hEst"));
+
+    // Convert rotation to radians, if necessary
+    {
+        Rot_ *= (mathematicalConstant::pi / 180.0);
+    }
+
+    // Initialize transformation tensors
+    globalToLocal_ = calcTensor( 1.0 * Rot_);
+    localToGlobal_ = calcTensor(-1.0 * Rot_);
+}
+
+
+sixDOFBody::odeSixDOF::odeSixDOF()
+:
+    coeffs_(6, 0.0)
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+sixDOFBody::~sixDOFBody()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+//- Calculate forces
+void sixDOFBody::calculateForces()
+{
+    // Fetch pressure from objectRegistry
+    const volScalarField& p = mesh_.lookupObject<volScalarField>(pName_);
+
+    // Fetch gravity from objectRegistry
+    vector g =
+    (
+        mesh_.lookupObject<uniformDimensionedVectorField>(gName_).value()
+    );
+
+    // Store forces / moments
+    for (label i = 2; i > 0; i--)
+    {
+        Fs_[i] = Fs_[i-1];
+        Ms_[i] = Ms_[i-1];
+    }
+
+    scalar sumWeightFactor = cmptSum(weightFactor_);
+
+    // Update force
+    Fs_[0] =
+    (
+        gSum
+        (
+            p.boundaryField()[patchIndex_]
+          * (
+                globalToLocal_
+              & mesh_.Sf().boundaryField()[patchIndex_]
+            )
+        )
+    );
+
+    // Update centre-of-gravity
+    Cg_ += (mesh_.time().deltaT().value() * localVelocity_);
+
+    // Update moment
+    Ms_[0] =
+    (
+        gSum
+        (
+            (
+                globalToLocal_
+              & (mesh_.Cf().boundaryField()[patchIndex_] - Cg_)
+            )
+          ^ (
+                p.boundaryField()[patchIndex_]
+              * (
+                    globalToLocal_
+                  & mesh_.Sf().boundaryField()[patchIndex_]
+                )
+            )
+        )
+    );
+
+    // Calculate average forces / moments
+    fAvg_ = vector::zero;
+    mAvg_ = vector::zero;
+
+    for (label j = 0; j < 3; j++)
+    {
+        for (label i = 0; i < 3; i++)
+        {
+            fAvg_[j] += (weightFactor_[i] * Fs_[i][j] / sumWeightFactor);
+            mAvg_[j] += (weightFactor_[i] * Ms_[i][j] / sumWeightFactor);
+        }
+    }
+
+    // Accumulate force
+    fTotal_ = (fAvg_ + (globalToLocal_ & (mass_ * g)));
+
+    // Write out a bit
+    Info<< " Force on body: "
+        << mesh_.boundaryMesh()[patchIndex_].name() << nl
+        << " Pressure force: " << Fs_[0] << nl
+        << " Moment: " << Ms_[0] << nl
+        << endl;
+}
+
+
+//- Balance forces
+void sixDOFBody::balanceForces()
+{
+    // Fetch time-step
+    scalar deltaT = mesh_.time().deltaT().value();
+
+    // Update total displacement / rotation
+    totalDisplacement_ += (deltaT * localOldVelocity_);
+    totalRotation_ += (rotation_ * 180.0 / mathematicalConstant::pi);
+
+    // Solve for translation
+    forAll(calcTransDOF_, dofI)
+    {
+        if (calcTransDOF_[dofI])
+        {
+            // Fetch coefficients
+            scalarField& y = ode_.coeffs();
+
+            scalar magTrans = mag(totalDisplacement_[dofI]);
+
+            // Set coefficients
+            y[0] = 0.0;
+            y[1] = localOldVelocity_[dofI];
+            y[2] = mass_;
+            y[3] = dTrans_[dofI];
+            y[4] = magTrans * kTrans_[dofI];
+            y[5] = fTotal_[dofI] + fConst_[dofI];
+
+            // Solve the ODE
+            odeSolver_->solve
+            (
+                0.0,
+                deltaT,
+                eps_,
+                hEst_
+            );
+
+            // Update velocity / displacement
+            localVelocity_[dofI] = y[1];
+            displacement_[dofI] = y[0];
+        }
+    }
+
+    // Write out a bit
+    Info<< " Velocity CG: " << localVelocity_ << nl
+        << " Total Displacement: " << totalDisplacement_ << nl
+        << endl;
+
+    // Update old information
+    localOldVelocity_ = localVelocity_;
+    globalVelocity_ = (localToGlobal_ & localVelocity_);
+
+    // Solve for rotation
+    forAll(calcRotDOF_, dofI)
+    {
+        if (calcRotDOF_[dofI])
+        {
+            // Fetch coefficients
+            scalarField& y = ode_.coeffs();
+
+            scalar magRot =
+            (
+                mag
+                (
+                    totalRotation_[dofI]
+                  * (mathematicalConstant::pi / 180.0)
+                )
+            );
+
+            // Set coefficients
+            y[0] = 0.0;
+            y[1] = localOldOmega_[dofI];
+            y[2] = J_[dofI];
+            y[3] = dRot_[dofI];
+            y[4] = magRot * kRot_[dofI];
+            y[5] = mAvg_[dofI] + mConst_[dofI];
+
+            // Solve the ODE
+            odeSolver_->solve
+            (
+                0.0,
+                deltaT,
+                eps_,
+                hEst_
+            );
+
+            // Update angular velocity / rotation
+            localOmega_[dofI] = y[1];
+            rotation_[dofI] = y[0];
+        }
+    }
+
+    // Write out a bit
+    Info<< " Angular Velocity CG: "
+        << localOmega_ * 180.0 / mathematicalConstant::pi << nl
+        << " Total Rotation: " << totalRotation_ << nl
+        << endl;
+
+    // Update old information
+    localOldOmega_ = localOmega_;
+    globalOmega_ = (localToGlobal_ & localOmega_);
+
+    // Now set displacement BCs for mesh motion
+    vectorField points(mesh_.boundaryMesh()[patchIndex_].localPoints() - Cg_);
+
+    vectorField Ux(((points ^ vector(1,0,0)) & globalOmega_) * vector(1,0,0));
+    vectorField Uy(((points ^ vector(0,1,0)) & globalOmega_) * vector(0,1,0));
+    vectorField Uz(((points ^ vector(0,0,1)) & globalOmega_) * vector(0,0,1));
+
+    // Calculate displacement for motionSolver
+    vectorField motionDisplacement
+    (
+        (Ux + Uy + Uz + globalVelocity_) * deltaT
+    );
+
+    pointField& refPoints = const_cast<pointField&>
+    (
+        mesh_.lookupObject<pointField>("refPoints")
+    );
+
+    // Assign boundary conditions to the motion solver
+    const labelList& meshPts = mesh_.boundaryMesh()[patchIndex_].meshPoints();
+
+    forAll(meshPts,pointI)
+    {
+        refPoints[meshPts[pointI]] += motionDisplacement[pointI];
+    }
+}
+
+
+void sixDOFBody::odeSixDOF::derivatives
+(
+    const scalar x,
+    const scalarField& y,
+    scalarField& dydx
+) const
+{
+    dydx[0] = y[1];
+    dydx[1] = (y[5]-y[3]*y[1]-y[4]*y[0]) / y[2];
+    dydx[2] = 0.0;
+    dydx[3] = 0.0;
+    dydx[4] = 0.0;
+    dydx[5] = 0.0;
+}
+
+
+void sixDOFBody::odeSixDOF::jacobian
+(
+    const scalar x,
+    const scalarField& y,
+    scalarField& dfdx,
+    scalarSquareMatrix& dfdy
+) const
+{
+    notImplemented("odeSixDOF::jacobian() const");
+}
+
+
+void sixDOFBody::odeSixDOF::update(const scalar delta)
+{
+
+}
+
+
+} // End namespace Foam
+
+// ************************************************************************* //
---
+++ sixDOF/sixDOFFoam.C
@@ -0,0 +1,193 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright held by original author
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Application
+    sixDOFFoam
+
+Description
+    Six-DOF solver
+
+Author
+    Sandeep Menon
+    University of Massachusetts Amherst
+    All rights reserved
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "sixDOFBody.H"
+#include "dynamicFvMesh.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+#   include "createTime.H"
+#   include "createDynamicFvMesh.H"
+#   include "initContinuityErrs.H"
+#   include "initTotalVolume.H"
+#   include "readGravitationalAcceleration.H"
+#   include "createFields.H"
+
+    // Create Six-DOF bodies
+    IOdictionary sixDOFDict
+    (
+        IOobject
+        (
+            "sixDOFDict",
+            runTime.constant(),
+            mesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+
+    // Read dictionary list of bodies
+    PtrList<dictionary> bodyDicts(sixDOFDict.lookup("bodies"));
+
+    // Initialize bodies
+    PtrList<sixDOFBody> bodies(bodyDicts.size());
+
+    forAll(bodies, bodyI)
+    {
+        bodies.set
+        (
+            bodyI,
+            new sixDOFBody
+            (
+                mesh,
+                sixDOFDict,
+                bodyDicts[bodyI],
+                bodyI
+            )
+        );
+    }
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+#       include "readControls.H"
+#       include "checkTotalVolume.H"
+#       include "CourantNo.H"
+
+#       include "setDeltaT.H"
+
+        // Make the fluxes absolute
+        fvc::makeAbsolute(phi, U);
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        bool meshChanged = mesh.update();
+
+#       include "volContinuity.H"
+
+        if (correctPhi && meshChanged)
+        {
+#           include "correctPhi.H"
+        }
+
+        // Make the fluxes relative to the mesh motion
+        fvc::makeRelative(phi, U);
+
+        if (meshChanged)
+        {
+#           include "CourantNo.H"
+        }
+
+#       include "UEqn.H"
+
+        // --- PISO loop
+
+        for (int corr=0; corr<nCorr; corr++)
+        {
+            rAU = 1.0/UEqn.A();
+
+            U = rAU*UEqn.H();
+            phi = (fvc::interpolate(U) & mesh.Sf());
+
+            adjustPhi(phi, U, p);
+
+            for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
+            {
+                fvScalarMatrix pEqn
+                (
+                    fvm::laplacian(rAU, p) == fvc::div(phi)
+                );
+
+                pEqn.setReference(pRefCell, pRefValue);
+
+                if (corr == nCorr - 1 && nonOrth == nNonOrthCorr)
+                {
+                    pEqn.solve(mesh.solver(p.name() + "Final"));
+                }
+                else
+                {
+                    pEqn.solve(mesh.solver(p.name()));
+                }
+
+                if (nonOrth == nNonOrthCorr)
+                {
+                    phi -= pEqn.flux();
+                }
+            }
+
+#           include "continuityErrs.H"
+
+            // Make the fluxes relative to the mesh motion
+            fvc::makeRelative(phi, U);
+
+            U -= rAU*fvc::grad(p);
+            U.correctBoundaryConditions();
+        }
+
+        // Calculate forces on all bodies
+        forAll(bodies, bodyI)
+        {
+            bodies[bodyI].calculateForces();
+        }
+
+        // Balance forces on all bodies
+        forAll(bodies, bodyI)
+        {
+            bodies[bodyI].balanceForces();
+        }
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return(0);
+}
+
+
+// ************************************************************************* //
